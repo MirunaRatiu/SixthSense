51fbd56a8c3ae0cad52b31d69dbd729247900e58
Molnar Sara <saraviviana68@gmail.com>
2025-04-23 19:45:02 +0300

    added cv deletion after a month feature

--- service-java/src/main/java/com/cv_jd_matching/HR/repository/ICvRepository.java
+++ service-java/src/main/java/com/cv_jd_matching/HR/repository/ICvRepository.java
@@ -1,9 +1,13 @@
 package com.cv_jd_matching.HR.repository;
 
 import com.cv_jd_matching.HR.entity.Cv;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Optional;
 
 @Repository
@@ -15,5 +19,9 @@ public interface ICvRepository extends CrudRepository<Cv, Integer> {
     Optional<Cv> findCvByPathName(String path);
 
     Optional<Cv> findCvByName(String name);
+
+    @Modifying
+    @Transactional
+    void deleteAllByCreatedAtBefore(Date date);
 
 }
